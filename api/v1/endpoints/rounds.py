51c827d1a5e194a0a7e88f2531e5082332fff529
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-11-26 11:33:37 -0300

    :bug: fix: removing _id from response

--- api/v1/endpoints/rounds.py
+++ api/v1/endpoints/rounds.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, status, Depends, HTTPException, Response
-from models.round import Round
+from models.round import RoundModel
 from models.game import Game
 from typing import List
 from services.scraping.rounds_scraping import scrap_rounds
 from database.database import get_round as get_round_db
 router = APIRouter()
 
-@router.get('/{num}', response_model = Round)
+@router.get('/{num}', response_model = RoundModel)
 async def get_round(num: int):
     if num < 0 or num > 38:
         raise HTTPException(detail = 'Por favor informe uma rodada válida: de 1 a 38', status_code = 400)
--- api/v1/endpoints/table.py
+++ api/v1/endpoints/table.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, status, Depends, HTTPException, Response
-from models.table import Table
+from models.table import TableModel
 from typing import List
-from services.scraping.table_scraping import scrap_table
 from database.database import get_table as get_table_db
 router = APIRouter()
 
-@router.get('/', response_model = List[Table])
-async def get_table():
+   
+@router.get('/')
+async def get_table(response_model = TableModel):
     table = get_table_db()
     table = await table.to_list()
-    return table
+    return format_table(table)
+
+def format_table(table_document):
+    return [TableModel(**dict(item)) for item in table_document]
